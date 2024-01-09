6ed845565198342e4d58096eb9ea89360f65f57f
unknown <luiscarlos81@gmail.com>
2024-01-08 18:56:58 -0600

    proyecto inicial

---
+++ proyectointegrador/src/test/java/dev/luismartinez/proyectointegrador/ProyectointegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luismartinez.proyectointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectointegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f02dc434c5e02f08ed4084b4d762cba78fab3942
unknown <luiscarlos81@gmail.com>
2024-01-11 21:38:56 -0600

    restructuracion

--- proyectointegrador/src/main/java/dev/luismartinez/proyectointegrador/ProyectointegradorApplication.java
+++ proyectointegrador/src/main/java/dev/luismartinez/proyectointegrador/ProyectointegradorApplication.java
@@ -1,4 +1,4 @@
-package controller.health;
+package dev.luismartinez.proyectointegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- proyectointegrador/src/main/java/dev/luismartinez/proyectointegrador/controller/health/HealthController.java
+++ proyectointegrador/src/main/java/dev/luismartinez/proyectointegrador/controller/health/HealthController.java
@@ -1,4 +1,4 @@
-package controller.health;
+package dev.luismartinez.proyectointegrador.controller.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
